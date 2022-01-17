26bdee29a620e1c8b18a9e88e15c3af3452e221a
Colt Thatcher <colt.thatcher303@gmail.com>
2022-01-17 12:08:53 -0800

    Took code from my recipe project and changed variable names problem seems to be fixxed

--- SweetTreat/Controllers/FlavorsController.cs
+++ SweetTreat/Controllers/FlavorsController.cs
@@ -5,10 +5,9 @@ using SweetTreat.Models;
 using System.Collections.Generic;
 using System.Linq;
 
-
 namespace SweetTreat.Controllers
 {
-public class FlavorsController : Controller
+  public class FlavorsController: Controller
   {
     private readonly SweetTreatContext _db;
 
@@ -34,6 +33,51 @@ public class FlavorsController : Controller
       _db.Flavors.Add(flavor);
       _db.SaveChanges();
       return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisFlavor = _db.Flavors
+        .Include(flavor => flavor.JoinEntities)
+        .ThenInclude(join => join.Treat)
+        .FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
+
+    public ActionResult Edit (int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Flavor flavor)
+    {
+      _db.Entry(flavor).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = flavor.FlavorId });
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      _db.Flavors.Remove(thisFlavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddTreat(int id)
+    {
+      var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "TreatName");
+      return View(thisFlavor);
     }
   }
 }
